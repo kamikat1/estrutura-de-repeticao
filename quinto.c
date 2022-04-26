b9f20401a404c1b7ff7be45924bd6284c32472c2
kamikat1 <88120995+kamikat1@users.noreply.github.com>
2022-04-26 12:28:45 -0300

    Fazendo o console imprimir valores entre 2 números
    Antes estava sendo impresso na tela valores entre 2 números incluindo o min e max, agora está correto

--- quinto.c
+++ quinto.c
@@ -1,17 +1,22 @@
+////Faça um programa que dados dois números mostre todos os números inteiros entre estes dois números.
+
 #include <stdio.h>
 #include <stdlib.h>
-//Faça um programa que dados dois números mostre todos os números inteiros entre estes dois números.
-int main(){
-        int f, l;
-        printf("digite um numero: ");
-        scanf("%d", &f);
-        printf("digite um numero: ");
-        scanf("%d", &l);
-        while(f<=l){
-            printf("%d, ", f);
-            f++;
-        }
-//deveria excluir os n das extremidades
 
-return 0;
+int main()
+{   int min, max, aux, count;
+    printf("digite 2 numeros: ");
+    scanf("%d %d", &min, &max);
+
+    if(min>max){
+        aux = min;
+        min=max;
+        max = min;
+    }
+    count=min+1;
+    for(count<min; count<max; count++){
+        printf("%d", count);
+    }
+
+    return 0;
 }
